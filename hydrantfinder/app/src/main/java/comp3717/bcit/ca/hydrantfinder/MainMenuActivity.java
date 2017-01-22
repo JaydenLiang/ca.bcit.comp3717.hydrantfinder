b5aff379c849ef26863370e67d83eededc9b1c2d
Jayden Liang <jaydenliang81@gmail.com>
2017-01-21 23:56:55 -0800

    add search address page and its searching logic, returning workflow.

--- hydrantfinder/app/src/main/java/comp3717/bcit/ca/hydrantfinder/MainMenuActivity.java
+++ hydrantfinder/app/src/main/java/comp3717/bcit/ca/hydrantfinder/MainMenuActivity.java
@@ -3,23 +3,21 @@ package comp3717.bcit.ca.hydrantfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = MainMenuActivity.class.getName();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
@@ -55,28 +53,6 @@ public class MainMenuActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -105,6 +81,12 @@ public class MainMenuActivity extends AppCompatActivity
     public void navigateToSetFilterActivity(final View view){
         Log.d(TAG, "enter navigateToSetFilterActivity");
         Intent intentToNavigate = new Intent(this, SetFilterActivity.class);
+        startActivity(intentToNavigate);
+    }
+
+    public void navigateToSearchAddressActivity(final View view){
+        Log.d(TAG, "enter navigateToSearchAddressActivity");
+        Intent intentToNavigate = new Intent(this, SearchAddressActivity.class);
         startActivity(intentToNavigate);
     }
 }
