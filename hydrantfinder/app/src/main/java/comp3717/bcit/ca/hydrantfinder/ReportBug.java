f6b24fd06ae594673a52db481bcc0ff19d4fdc65
Junnan Tang <junnan.tang.2014@gmail.com>
2017-03-09 11:33:53 -0800

    send report to email

--- hydrantfinder/app/src/main/java/comp3717/bcit/ca/hydrantfinder/ReportBug.java
+++ hydrantfinder/app/src/main/java/comp3717/bcit/ca/hydrantfinder/ReportBug.java
@@ -1,21 +1,35 @@
 package comp3717.bcit.ca.hydrantfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ReportBug extends AppCompatActivity {
-
+    EditText msg;
+    Button sendEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_bug);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sendEmail=(Button)findViewById(R.id.btn_send);
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg=(EditText)findViewById(R.id.input_bug_description);
+                String message = msg.getText().toString();
+                sendEmail(message);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +44,16 @@ public class ReportBug extends AppCompatActivity {
     public void sendReport(final View view){
         Toast.makeText(this, "Sending Report...", Toast.LENGTH_SHORT).show();
         finish();
+    }
+    protected void sendEmail(String message){
+        String[] to = new String[]{"juntrc@gmail.com"};
+        String subject=("A message from your app!");
+        Intent emailIntent=new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL,to);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent,"Email"));
     }
 
 }
